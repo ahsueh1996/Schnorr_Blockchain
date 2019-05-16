dee3a8f2738e20851043430eb8fdd01c976e93e8
Pham Phu Doan <1212087@student.hcmus.edu.vn>
2019-05-16 13:50:07 +0700

    update folder structure

--- server/blockchain.py
+++ server/blockchain.py
@@ -34,14 +34,8 @@ from urllib.parse import urlparse
 from uuid import uuid4
 
 import requests
-from flask import Flask, jsonify, request, render_template
-from flask_cors import CORS
 
-
-
-MINING_SENDER = "THE BLOCKCHAIN"
-MINING_REWARD = 1
-MINING_DIFFICULTY = 2
+import config as conf
 
 
 class Blockchain:
@@ -92,7 +86,7 @@ class Blockchain:
                                     'value': value})
 
         #Reward for mining a block
-        if sender_address == MINING_SENDER:
+        if sender_address == conf.MINING_SENDER:
             self.transactions.append(transaction)
             return len(self.chain) + 1
         #Manages transactions from wallet to another wallet
@@ -146,7 +140,7 @@ class Blockchain:
         return nonce
 
 
-    def valid_proof(self, transactions, last_hash, nonce, difficulty=MINING_DIFFICULTY):
+    def valid_proof(self, transactions, last_hash, nonce, difficulty=conf.MINING_DIFFICULTY):
         """
         Check if a hash value satisfies the mining conditions. This function is used within the proof_of_work function.
         """
@@ -178,7 +172,7 @@ class Blockchain:
             transaction_elements = ['sender_address', 'recipient_address', 'value']
             transactions = [OrderedDict((k, transaction[k]) for k in transaction_elements) for transaction in transactions]
 
-            if not self.valid_proof(transactions, block['previous_hash'], block['nonce'], MINING_DIFFICULTY):
+            if not self.valid_proof(transactions, block['previous_hash'], block['nonce'], conf.MINING_DIFFICULTY):
                 return False
 
             last_block = block
@@ -218,138 +212,5 @@ class Blockchain:
 
         return False
 
-# Instantiate the Node
-app = Flask(__name__)
-CORS(app)
-
 # Instantiate the Blockchain
 blockchain = Blockchain()
-
-@app.route('/')
-def index():
-    return render_template('./index.html')
-
-@app.route('/configure')
-def configure():
-    return render_template('./configure.html')
-
-
-
-@app.route('/transactions/new', methods=['POST'])
-def new_transaction():
-    values = request.form
-
-    # Check that the required fields are in the POST'ed data
-    required = ['sender_address', 'recipient_address', 'amount', 'signature']
-    if not all(k in values for k in required):
-        return 'Missing values', 400
-    # Create a new Transaction
-    transaction_result = blockchain.submit_transaction(values['sender_address'], values['recipient_address'], values['amount'], values['signature'])
-
-    if transaction_result == False:
-        response = {'message': 'Invalid Transaction!'}
-        return jsonify(response), 406
-    else:
-        response = {'message': 'Transaction will be added to Block '+ str(transaction_result)}
-        return jsonify(response), 201
-
-@app.route('/transactions/get', methods=['GET'])
-def get_transactions():
-    #Get transactions from transactions pool
-    transactions = blockchain.transactions
-
-    response = {'transactions': transactions}
-    return jsonify(response), 200
-
-@app.route('/chain', methods=['GET'])
-def full_chain():
-    response = {
-        'chain': blockchain.chain,
-        'length': len(blockchain.chain),
-    }
-    return jsonify(response), 200
-
-@app.route('/mine', methods=['GET'])
-def mine():
-    # We run the proof of work algorithm to get the next proof...
-    last_block = blockchain.chain[-1]
-    nonce = blockchain.proof_of_work()
-
-    # We must receive a reward for finding the proof.
-    blockchain.submit_transaction(sender_address=MINING_SENDER, recipient_address=blockchain.node_id, value=MINING_REWARD, signature="")
-
-    # Forge the new Block by adding it to the chain
-    previous_hash = blockchain.hash(last_block)
-    block = blockchain.create_block(nonce, previous_hash)
-
-    response = {
-        'message': "New Block Forged",
-        'block_number': block['block_number'],
-        'transactions': block['transactions'],
-        'nonce': block['nonce'],
-        'previous_hash': block['previous_hash'],
-    }
-    return jsonify(response), 200
-
-
-
-@app.route('/nodes/register', methods=['POST'])
-def register_nodes():
-    values = request.form
-    nodes = values.get('nodes').replace(" ", "").split(',')
-
-    if nodes is None:
-        return "Error: Please supply a valid list of nodes", 400
-
-    for node in nodes:
-        blockchain.register_node(node)
-
-    response = {
-        'message': 'New nodes have been added',
-        'total_nodes': [node for node in blockchain.nodes],
-    }
-    return jsonify(response), 201
-
-
-@app.route('/nodes/resolve', methods=['GET'])
-def consensus():
-    replaced = blockchain.resolve_conflicts()
-
-    if replaced:
-        response = {
-            'message': 'Our chain was replaced',
-            'new_chain': blockchain.chain
-        }
-    else:
-        response = {
-            'message': 'Our chain is authoritative',
-            'chain': blockchain.chain
-        }
-    return jsonify(response), 200
-
-
-@app.route('/nodes/get', methods=['GET'])
-def get_nodes():
-    nodes = list(blockchain.nodes)
-    response = {'nodes': nodes}
-    return jsonify(response), 200
-
-
-
-if __name__ == '__main__':
-    from argparse import ArgumentParser
-
-    parser = ArgumentParser()
-    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
-    args = parser.parse_args()
-    port = args.port
-
-    app.run(host='127.0.0.1', port=port)
-
-
-
-
-
-
-
-
