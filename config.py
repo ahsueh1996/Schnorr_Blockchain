112a4101bb5649829299dbe1a5c50659b5dae0b9
ahsueh1996 <ahsueh1996@gmail.com>
2020-04-14 14:10:47 -0400

    ecdsa 5 diff

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 ROOT_DIR = os.getcwd()
 
-DIGITAL_SIGNATURE_ALGO = 'SCHNORR'
+DIGITAL_SIGNATURE_ALGO = 'ECDSA'
 HASH_ALGO = 'SHA256'
 
 IGNORE = 0
@@ -14,9 +14,9 @@ LOG_FILE = "./log.txt"
 
 
 MINING_REWARD = 1
-MINING_DIFFICULTY = 4
+MINING_DIFFICULTY = 5
 
-BLOCK_LIMIT = 90
+BLOCK_LIMIT = 10
 TRANSACTION_RATE = 15 # TRANS_RATE x (nodes in network) / (avg mining time) = load per unit time that the network is dealing with
 NOUNCE_DISTANCE = 10000
 
@@ -25,7 +25,7 @@ NODE_CSV_FILE = "./node/node_list.csv"
 CONNECTION_ADAPTER = "http://"
 
 MASTER="155.138.145.252:5000"
-END_OF_CHAIN = 50
+END_OF_CHAIN = 15
 
 
 
